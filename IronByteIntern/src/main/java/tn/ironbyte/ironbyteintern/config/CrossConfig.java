2aab4fbb02f4eede63be81ab9eab6124bbf1b1b8
Soumayabderahmen <soumaya.abderahmen@esprit.tn>
2024-08-02 00:18:58 +0100

    Set up jenkinsFile..

--- IronByteIntern/src/main/java/tn/ironbyte/ironbyteintern/config/CrossConfig.java
+++ IronByteIntern/src/main/java/tn/ironbyte/ironbyteintern/config/CrossConfig.java
@@ -10,13 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CrossConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        registry.addMapping("/api/**") // Correct mapping pattern
+                .allowedOrigins("http://localhost:4200", "http://localhost:4300") // Include both URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-               
     }
 
 
